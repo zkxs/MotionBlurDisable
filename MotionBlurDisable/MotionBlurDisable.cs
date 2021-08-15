767f01cca203f92a9fad2a6398d75c40924b29fb
Michael Ripley <zkxs00@gmail.com>
2021-08-15 01:25:20 -0500

    Switch from MelonLoader to NeosModLoader

--- MotionBlurDisable/MotionBlurDisable.cs
+++ MotionBlurDisable/MotionBlurDisable.cs
@@ -1,5 +1,5 @@
 ﻿using HarmonyLib;
-using MelonLoader;
+using NeosModLoader;
 using System;
 using System.Reflection;
 using UnityEngine;
@@ -7,22 +7,27 @@ using UnityEngine.Rendering.PostProcessing;
 
 namespace MotionBlurDisable
 {
-    public class MotionBlurDisable : MelonMod
+    public class MotionBlurDisable : NeosMod
     {
+        public override string Name => "MotionBlurDisable";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/MotionBlurDisable";
+
         private static bool _first_trigger = false;
 
-        public override void OnApplicationStart()
+        public override void OnEngineInit()
         {
-            HarmonyLib.Harmony harmony = new HarmonyLib.Harmony("net.michaelripley.MotionBlurDisable");
+            Harmony harmony = new Harmony("net.michaelripley.MotionBlurDisable");
             MethodInfo originalMethod = AccessTools.DeclaredMethod(typeof(CameraInitializer), nameof(CameraInitializer.SetPostProcessing), new Type[] { typeof(Camera), typeof(bool), typeof(bool), typeof(bool) });
             if (originalMethod == null)
             {
-                MelonLogger.Error("Could not find CameraInitializer.SetPostProcessing(Camera, bool, bool, bool) setter");
+                Error("Could not find CameraInitializer.SetPostProcessing(Camera, bool, bool, bool)");
                 return;
             }
             MethodInfo replacementMethod = AccessTools.DeclaredMethod(typeof(MotionBlurDisable), nameof(SetPostProcessing));
             harmony.Patch(originalMethod, prefix: new HarmonyMethod(replacementMethod));
-            MelonLogger.Msg("Hook installed successfully");
+            Msg("Hook installed successfully");
         }
 
         private static bool SetPostProcessing(Camera c, bool enabled, bool motionBlur, bool screenspaceReflections)
@@ -43,7 +48,7 @@ namespace MotionBlurDisable
             if (!_first_trigger)
             {
                 _first_trigger = true;
-                MelonLogger.Msg("Hook triggered! Everything worked!");
+                Msg("Hook triggered! Everything worked!");
             }
 
             return false; // skip original method
